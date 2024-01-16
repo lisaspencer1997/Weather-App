8d4f60daf499efbf3ac88ddc0e170bb57d38250e
Mona Lisa <lisa-spencer@outlook.com>
2024-01-16 17:22:08 +0000

    created html, css, javascript file, and started readme file, also started pseudocode for what i want to see visually:

---
+++ README.md
@@ -0,0 +1,33 @@
+# Week 8 Challenge: Weather App
+
+## Project status
+Started on Fri 12 Jan 18:23.
+
+## Name
+Lisa Spencer: Weather App
+
+## Description
+This page is the challenge/homework for Week 8 of my Bootcamp course. The aim is to create a weather app using advanced javascript and utilising server APIs.
+
+
+## Link to deployed application
+https://lisaspencer1997.github.io/Work-Day-Scheduler/code/index.html
+
+## Screen Recordings
+This screen recording shows ...
+
+![](ScreenRecording1.gif)
+
+This screen recording shows ...
+
+![](ScreenRecording2.gif)
+
+## Support
+Contact me on lisa-spencer@outlook.com if something is not working as intended.
+
+## Authors and acknowledgment
+* 
+
+
+## License
+This is licenced through MIT. 
---
+++ script.js
@@ -0,0 +1,32 @@
+
+// api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
+
+// e610d1ee0c1652aebf27c6bf71a547e3
+
+// https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}.
+
+// & What I want to see visually
+
+    // * Set up normal html components (inc. head, header, body etc) make sure to attach bootstrap (if using) and connect css and javascript files to the html
+    
+    // * On the page we should see:
+        // ? Title block with 'Weather Dashboard' in the middle
+
+        // ? Left block
+            // Header: 'Search for a city', with search bar underneath where user's would be able to type inside
+            // Below should then list previous entries made (store in local storage)
+            // The previous entries should be available as buttons that can be clicked again rather than retyping the search entry box
+
+        // ? Middle/right block
+            //First box should display title of searched for city followed by today's date and an icon displaying a visual representation of the current weather forecast in that city
+            //Underneath should list the local temperature, wind speed and humidity
+
+        // ? Lower middle blocks
+            //Header: 5-Day Forecast
+            //Should display 5 cards with the following 5 days in order
+            //Each card should display:
+                // ^the date
+                // ^an icon representing the weather
+                // ^temperature
+                // ^wind speed
+                // ^humidity
